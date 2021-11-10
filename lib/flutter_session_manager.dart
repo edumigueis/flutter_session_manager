52a2c8a4d5cb08fd74308568d28724b475a51205
Eduardo Migueis <edmigueis@hotmail.com>
2021-11-10 18:06:34 -0300

    Data Setter

--- lib/flutter_session_manager.dart
+++ lib/flutter_session_manager.dart
@@ -1,7 +1,76 @@
 library flutter_session_manager;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FlutterSessionManager {
+  /// Initialize session wrapper
+  final Map _session = {};
+
+  // SharedPreferences object to store persistent data
+  SharedPreferences? prefs;
+
+  // Access and initialize the SharedPrferences instance
+  Future _accessSharedPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  /// Item setter
+  ///
+  /// @param key String
+  /// @param value any
+  /// @returns Future
+  Future set(key, value) async {
+    await _accessSharedPrefs();
+    try {
+      // Detect item type
+      switch (value.runtimeType) {
+        // String
+        case String:
+          {
+            prefs!.setString(key, value);
+          }
+          break;
+
+        // Integer
+        case int:
+          {
+            prefs!.setInt(key, value);
+          }
+          break;
+
+        // Boolean
+        case bool:
+          {
+            prefs!.setBool(key, value);
+          }
+          break;
+
+        // Double
+        case double:
+          {
+            prefs!.setDouble(key, value);
+          }
+          break;
+
+        // List<String>
+        case List:
+          {
+            prefs!.setStringList(key, value);
+          }
+          break;
+
+        // Object
+        default:
+          {
+            prefs!.setString(key, jsonEncode(value.toJson()));
+          }
+      }
+    } catch (e) {
+      throw Exception("Key or value are not the correct type.");
+    }
+
+    // Add item to session wrapper
+    _session.putIfAbsent(key, () => value);
+  }
 }
